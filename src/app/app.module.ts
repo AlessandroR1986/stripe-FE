4ac8e79964d2bb2a4ba258bb552f5b4c13542887
Alessandro <a.ricci@softwareindustriale.it>
2024-03-28 12:38:10 +0100

    form di pagamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxStripeModule } from 'ngx-stripe';
+import { environment } from './environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
+import { HttpClientModule } from  '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxStripeModule.forRoot(environment.stripe.publicKey),
+    ReactiveFormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production : false,
+    apiUrl: 'http://localhost:8080',
+    stripe : {
+        publicKey: 'pk_test_51OycrvCATvflhqx11RK7H56bYmmUSXvDEv8Syo9qxfuXmtdUtCmgALIG0QUqsB3FLteINAP6f82sk2CiaWxmd5u800XhYYYjdq'
+    }
+}
